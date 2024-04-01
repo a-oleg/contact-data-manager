5d6a7c3c10b5dd92ffa5ed0c99d757390dcdcf2b
Oleg <oleg.arhipovski@gmail.com>
2024-04-01 20:47:18 +0300

    First commit

---
+++ src/main/java/contactdatamanager/aoleg/com/github/ContactDataManagerApplication.java
@@ -0,0 +1,13 @@
+package contactdatamanager.aoleg.com.github;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContactDataManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContactDataManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/contactdatamanager/aoleg/com/github/ContactDataManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package contactdatamanager.aoleg.com.github;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContactDataManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
